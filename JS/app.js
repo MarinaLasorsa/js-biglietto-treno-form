5776d5e34f0bc733ef9f71320a17a828ec8ab2d9
MarinaLasorsa <marina.lasorsa@gmail.com>
2024-03-05 18:22:49 +0100

    EDIT: changed age input into select, created constant for select element, changed conditions for discount based on age value

--- JS/app.js
+++ JS/app.js
@@ -5,12 +5,17 @@ Usando esclusivamente due input:
 un input per i km e un input per l’età, e un bottone.
 Cliccando sul bottone recuperate i valori delle input*/
 
-//1. dichiarare e selezionare gli elementi di input kilometers e age come variabili
+//1. dichiarare e selezionare elemento di input kilometers come variabile
 const inputKilometersElement = document.getElementById("kilometers");
 console.log(inputKilometersElement);
 
-const inputAgeElement = document.getElementById("age");
-console.log(inputAgeElement);
+//Dichiarare elemento select
+//const inputAgeElement = document.getElementById("age");
+//console.log(inputAgeElement);
+
+const selectAgeElement = document.getElementById("age");
+console.log(selectAgeElement);
+
 
 //2. dichiarare e selezionare l'elemento di tipo submit
 const submitElement = document.getElementById("submit");
@@ -21,7 +26,7 @@ console.log(submitElement);
 submitElement.addEventListener("click", function () {
 
     let kilometers = parseInt(inputKilometersElement.value); //number
-    let age = parseInt(inputAgeElement.value); //number
+    let age = selectAgeElement.value; //string
 
     console.log(kilometers, age);
 
@@ -37,16 +42,16 @@ submitElement.addEventListener("click", function () {
 
     let discount = 0;
 
-    //3. SE variabile age è meno di 18, 
+    //3. SE variabile age è "minor", 
 
-    if (age < 18) {
+    if (age === "minor") {
 
         //lo sconto è del 20% quindi prezzo iniziale moltiplicato per 0.2
         discount = basePrice * 0.2;
     }  
 
-    //4. ALTRIMENTI SE variabile age è maggiore di 65,
-    else if (age > 65) {
+    //4. ALTRIMENTI SE variabile age è "senior",
+    else if (age === "senior") {
 
         //lo sconto è del 40% quindi prezzo iniziale moltiplicato per 0.4
         discount = basePrice * 0.4;
